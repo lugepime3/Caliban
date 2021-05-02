db231cc45f6e5fd140f808d5d8124ae6d8d1adbf
Luis Gerardo Pino <lugepime3@gmail.com>
2021-05-02 20:42:48 -0300

    Se agregan mas detalles  de la API en  el fromulario de Swagger

--- src/main/java/com/morlock/caliban/controllers/impl/MutantControllerImpl.java
+++ src/main/java/com/morlock/caliban/controllers/impl/MutantControllerImpl.java
@@ -12,8 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/com/morlock/caliban/swagger/SwaggerConfig.java
+++ src/main/java/com/morlock/caliban/swagger/SwaggerConfig.java
@@ -25,11 +25,15 @@ public class SwaggerConfig {
 
 	private ApiInfo getApiInfo() {
 		return new ApiInfo("Caliban-API", // NAME
-				"Detector del Gen Mutante", // DESCRIPTION
+				"Magneto quiere reclutar la mayor cantidad de mutantes para poder luchar contra los X-Men.\r\n" + 
+				"Esta es la version de una API que detecta si un humano es mutante basándose en su secuencia de ADN.\r\n" + 
+				"Recibe como parametro un array de Strings que representan cada fila de una tabla de (NxN) con la secuencia del ADN. \r\n" + 
+				"Las letras de los Strings solo pueden ser: (A,T,C,G), las cuales representa cada base nitrogenada del ADN.\r\n" + 
+				"<strong>Se sabra si un humano es mutante, si la API encuentra más de una secuencia de cuatro letras iguales​, de forma oblicua, horizontal o vertical.</strong>", // DESCRIPTION
 				"V. 1.0", // VERSION
 				"TERMS OF SERVICE URL", // TERMS OF SERVICE URL
-				new Contact("Luis G. Pino", "*cambiar*", "lugepime3@gmail.com"), "Test-Api", // LISENSE
-				"*cambiar*" // TERMS OF LICENSE URL
+				new Contact("Luis Gerardo Pino Mendoza", "https://github.com/lugepime3/Caliban", "lugepime3@gmail.com"), "", // LISENSE
+				"" // TERMS OF LICENSE URL
 		);
 	}
 
