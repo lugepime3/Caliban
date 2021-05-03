b96ec87843695faf25e6ceef3dba48f874260729
Luis Gerardo Pino <lugepime3@gmail.com>
2021-05-02 21:01:05 -0300

    Se agrega set de datos en la descripcion del API

--- src/main/java/com/morlock/caliban/swagger/SwaggerConfig.java
+++ src/main/java/com/morlock/caliban/swagger/SwaggerConfig.java
@@ -29,7 +29,10 @@ public class SwaggerConfig {
 				"Esta es la version de una API que detecta si un humano es mutante basándose en su secuencia de ADN.\r\n" + 
 				"Recibe como parametro un array de Strings que representan cada fila de una tabla de (NxN) con la secuencia del ADN. \r\n" + 
 				"Las letras de los Strings solo pueden ser: (A,T,C,G), las cuales representa cada base nitrogenada del ADN.\r\n" + 
-				"<strong>Se sabra si un humano es mutante, si la API encuentra más de una secuencia de cuatro letras iguales​, de forma oblicua, horizontal o vertical.</strong>", // DESCRIPTION
+				"<strong>Se sabra si un humano es mutante, si la API encuentra más de una secuencia de cuatro letras iguales​, de forma oblicua, horizontal o vertical.</strong>"+
+				"<p>Ejemplos:</p>"+
+				"<p><strong>Mutante:</strong> <i>[ \"ATGCGA\",\"CAGTGC\",\"TTATGT\",\"AGAAGG\",\"CCCCTA\",\"TCACTG\" ]</i></p>"+
+				"<p><strong>Humano :</strong> <i>[ \"TTGCTA\",\"CAGTGC\",\"TTATGT\",\"AGAAGG\",\"TCCCTA\",\"TCACTG\" ]</i></p>", // DESCRIPTION
 				"V. 1.0", // VERSION
 				"TERMS OF SERVICE URL", // TERMS OF SERVICE URL
 				new Contact("Luis Gerardo Pino Mendoza", "https://github.com/lugepime3/Caliban", "lugepime3@gmail.com"), "", // LISENSE
